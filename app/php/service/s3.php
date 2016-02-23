3f63cd6d896570e9f6469eeaadec1463ac1b1922
lhaggarty <lhaggarty@me.com>
2016-02-23 12:54:41 -0500

    configuration

---
+++ app/php/service/s3.php
@@ -0,0 +1,108 @@
+<?php
+
+include_once(dirname(dirname(__FILE__)) . '/include/loadutils.php');
+load_utils('id','date','path','log');
+
+require('aws/vendor/autoload.php'); // pull in aws-sdk
+
+function s3_file_source($input, $config){
+  return array(
+    'name' => $config['import_original_basename'],
+    'extension' => $input['extension'],
+    'type' => 'http',
+    'host' => $config['user_media_host'],
+    'path' => path_concat(path_concat($config['user_media_url'], $input['uid']), $input['id']),
+  );
+}
+function s3_file_import_url($import, $config){
+  $uid = (empty($import['uid']) ? '' : $import['uid']);
+  return path_concat('http://' . $config['user_media_host'], path_concat($config['user_media_url'], $uid));
+}
+function s3_file_export_url($import, $config){
+ return s3_file_import_url($import, $config);
+}
+function s3_file_config_defaults($config){
+  if (empty($config['s3_acl'])) $config['s3_acl'] = 'public-read';
+  if (empty($config['s3_expires'])) $config['s3_expires'] = '+ 1 hour';
+  if (empty($config['s3_region'])) $config['s3_region'] = '';
+  return $config;
+}
+function s3_file_config_error($config = array()){
+  $err = '';
+  if ((! $err) && empty($config['s3_bucket'])) $err = 'Configuration option s3_bucket required';
+  if ((! $err) && empty($config['user_media_host'])) $err = 'Configuration option user_media_host required';
+  if (! $err){
+    if (substr($config['user_media_host'], 0, strlen($config['s3_bucket'])) != $config['s3_bucket']) {
+      if (empty($config['user_media_directory'])) $err = 'Configuration option user_media_directory required if user_media_host does not begin with s3_bucket';
+      else if (substr($config['user_media_directory'], 0, strlen($config['s3_bucket'])) != $config['s3_bucket']) {
+        $err = 'Either user_media_host or user_media_directory must begin with s3_bucket';
+      }
+    }
+  }
+  return $err;
+}
+function s3_file_export_module_source($config){
+  return array(
+    'host' => $config['module_host'],
+    'directory' => $config['module_directory'],
+    'method' => 'get',
+    'type' => 'http'
+  );
+}
+function s3_file_destination($output, $config, $prefix = 's3'){
+  return array(
+    'type' => $prefix,
+    'bucket' => $config[$prefix . '_bucket'],
+    'region' => $config[$prefix . '_region'],
+    'path' => path_concat(path_concat($config['user_media_url'], $output['uid']), $output['id']),
+  );
+}
+function s3_file_import_init($import, $config) {
+		$response = array();
+		$err = '';
+		if (! $err){
+    $response['api'] = array();
+    $response['data'] = array();
+    $response['method'] = 'put';
+
+    $id = id_unique();
+    $bucket = $config['s3_bucket'];
+    $region = ($config['s3_region'] ? $config['s3_region'] : 'us-east-1');
+    $key = path_strip_slash_start($config['user_media_directory']);
+    // remove bucket name from url if it's there
+    if (substr($key, 0, strlen($bucket)) == $bucket) $key = substr($key, strlen(path_add_slash_end($bucket)));
+    $key = path_concat(path_concat(path_concat($key, $import['uid']), $id), $config['import_original_basename'] . '.' . $import['extension']);
+    $options_array = [
+      'version' => 'latest',
+      'region'  => $region
+    ];
+    if ($config['aws_secret_access_key'] && $config['aws_access_key_id']) {
+      $options_array['credentials'] = [];
+      $options_array['credentials']['key'] = $config['aws_access_key_id'];
+      $options_array['credentials']['secret'] = $config['aws_secret_access_key'];
+    }
+    try {
+      $s3 = new Aws\S3\S3Client($options_array);
+      $cmd = $s3->getCommand('PutObject', [
+        'Bucket' => $config['s3_bucket'],
+        'Key'    => $key,
+        'ACL' => $config['s3_acl'],
+        'ContentType' => $import['mime'],
+      ]);
+      $request = $s3->createPresignedRequest($cmd, $config['s3_expires']);
+      // Get the actual presigned-url
+      $response['endpoint'] = $presignedUrl = (string) $request->getUri();
+    } catch (Exception $e) {
+      $err = $e->getMessage();
+    }
+    // data key children all become part of the POST file upload request
+    $response['api']['id'] = $id;
+    $response['api']['type'] = $import['type'];
+    $response['api']['extension'] = $import['extension'];
+    $response['api']['label'] = $import['label'];
+  }
+  if ($err) $response['error'] = $err;
+  return $response;
+}
+
+
